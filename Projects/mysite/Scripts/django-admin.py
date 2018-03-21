245422fcbb753043cf209f73a53e81e27cba8319
BillyDavies1122 <36976243+BillyDavies1122@users.noreply.github.com>
2018-03-21 09:44:44 +0930

    Created virtual env

---
+++ Projects/mysite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\billy\documents\github\hit237\projects\mysite\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
